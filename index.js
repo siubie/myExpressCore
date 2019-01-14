8cfb29abbd1bdad6766c70d13fb443f1ade1d8b5
Putra Prima Arhandi <putraprima@gmail.com>
2019-01-15 04:47:28 +0700

    ganti pake hapi

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-// app.js
 const express = require("express");
-// Create Express app
-const app = express();
-// A sample route
-app.get("/", (req, res) => res.send("Hello World!"));
-// Start the Express server
-app.listen(3000, () => console.log("Server running on port 3000!"));
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,36 @@
-// app.js
-const express = require("express");
-// Create Express app
-const app = express();
-// A sample route
-app.get("/", (req, res) => res.send("Hello World!"));
-// Start the Express server
-app.listen(3000, () => console.log("Server running on port 3000!"));
+"use strict";
+
+const hapi = require("hapi");
+
+const server = hapi.Server({
+  port: 3000,
+  host: "localhost"
+});
+
+server.route({
+  method: "GET",
+  path: "/",
+  handler: (request, h) => {
+    return "Hello, world!";
+  }
+});
+
+server.route({
+  method: "GET",
+  path: "/{name}",
+  handler: (request, h) => {
+    return "Hello, " + encodeURIComponent(request.params.name) + "!";
+  }
+});
+
+const init = async () => {
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
+};
+
+process.on("unhandledRejection", err => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
